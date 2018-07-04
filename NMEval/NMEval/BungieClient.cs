a3d7c29cc29ba9f068a2a90386eb012a67feb333
mynameisfury <jonathanweatherall0@gmail.com>
2018-07-04 01:08:09 -0500

    client created

---
+++ NMEval/NMEval/BungieClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace NMEval
+{
+    public class BungieClient
+    {
+         public void QueryApi()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-Key", "YOUR-API-KEY-HERE");
+
+                var response = client.GetAsync("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/").Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                dynamic item = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+
+                Console.WriteLine(item.Response.data.inventoryItem.itemName); //Gjallarhorn
+            }
+        }
+    }
+}
